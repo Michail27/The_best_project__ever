a9baa07bd639382e9f2b384295ba1dc9e87d2cf3
Michail27 <michail27.03@mail.ru>
2020-12-02 21:26:56 +0300

    add models books with comment add some referense and >>>

--- manager/urls.py
+++ manager/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from manager.views import hello, bui
+from manager.views import hello, bui, MyPage
 
 urlpatterns =[
+    path("hello/<int:digit>/", hello),
+    path('hello/<str:name>/', hello),
     path('hello/', hello),
-    path('goodbye/', bui)
+    path('goodbye/', bui),
+    path('', MyPage.as_view(), name='the-main-page')
 
 ]
 
--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-def hello(request):
-    return HttpResponse('Hello world')
+from manager.models import Book
+
+
+def hello(request, name="filipp", digit =None):
+    if digit is not None:
+        return HttpResponse(f"digit is {digit}")
+    return HttpResponse(f'Hello {name}')
 
 def bui(request):
     return HttpResponse('Goodbye')
 # Create your views here.
+
+class MyPage(View):
+    def get(self, request):
+        context = {'books': Book.objects.all()}
+        return render(request, 'index.html', context)
